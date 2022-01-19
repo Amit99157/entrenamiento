7bf51489ac27ba93347879ec87221d1d6e099c64
Amit Singh <amit9915783@gmail.com>
2022-01-19 09:08:14 +0100

    Añadido For2.

---
+++ For2/src/com/amit/Main.java
@@ -0,0 +1,13 @@
+package com.amit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int n = 1000;
+        for(int i = 0; i < 5;)
+
+
+
+    }
+}
